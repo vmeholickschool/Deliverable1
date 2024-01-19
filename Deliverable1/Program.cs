3b7cf94e9012cfe05c763cf2c3bb1c1a7a87c31e
Meholick, Vicki <vickimeholick@rocketmortgage.com>
2024-01-19 09:46:49 -0500

    Fixed spelling and restock variable use.

--- Deliverable1/Program.cs
+++ Deliverable1/Program.cs
@@ -17,18 +17,43 @@ class Program
         //Title to user
         Console.WriteLine("Welcome to the restocking tool!");
 
-        //Question to user for number of of items purchased
-        Console.WriteLine("How many soda's have been sold today? " +  sodaStock + " are in stock.");
-        int sodasSold = int.Parse(Console.ReadLine());
+        //Question to user for number of of soda purchased
+        Console.WriteLine("How many sodas have been sold today? " +  sodaStock + " are in stock.");
+        int sodaSold = int.Parse(Console.ReadLine());
+        
 
+        //if user enters number over stock available
+        while (sodaSold > sodaStock)
+        {
+            Console.WriteLine("You have entered an invalid number, please enter a number less than or equal to " + sodaStock + ".");
+            sodaSold = int.Parse(Console.ReadLine());
+        }
+
+        //Question to user for number of chips sold
         Console.WriteLine("How many chips have been sold today? " + chipsStock + " are in stock.");
         int chipsSold = int.Parse(Console.ReadLine());
+        
 
+        // if user enters number over stock available
+        while (chipsSold > chipsStock)
+        {
+            Console.WriteLine("You have entered an invalid number, please enter a number less than or equal to " + chipsStock + ".");
+            chipsSold = int.Parse(Console.ReadLine());
+        }
+
+        //Question to user for number of candy sold
         Console.WriteLine("How many candies have been sold today? " + candyStock + " are in stock.");
         int candySold = int.Parse(Console.ReadLine());
 
+        // if user enters number of stock available
+        while (candySold > candyStock)
+        {
+            Console.WriteLine("You have entered an invalid number, please enter a number less than or equal to " + candyStock + ".");
+            candySold = int.Parse(Console.ReadLine());
+        }
+
         // Remaining stock calculation
-        int sodaRemaining = sodaStock - sodasSold;
+        int sodaRemaining = sodaStock - sodaSold;
         int chipsRemaining = chipsStock - chipsSold;
         int candyRemaining = candyStock - candySold;
 
@@ -48,7 +73,7 @@ class Program
         {
             Console.WriteLine(" There are " + chipsRemaining + " chips remaining, no restock needed.");
         }
-        if (candyRemaining <= candy)
+        if (candyRemaining <= candyRestock)
         {
             Console.WriteLine("There are " + candyRemaining + " candies remaining, please restock.");
         }
